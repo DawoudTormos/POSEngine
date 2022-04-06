6276371df975a2635a0bc76c87b6abe804fc232d
Dawoud <dztormos80@gmail.com>
2022-04-06 04:16:35 +0300

    Disabling the bowser cache by adding  meta tags in the 3 layouts views

--- resources/views/layouts/Layout.blade.php
+++ resources/views/layouts/Layout.blade.php
@@ -6,6 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+     <!-- Disabling browser cache-->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+
+    
+    
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     {{--<link href="{{ asset('css/search-bar.css') }}" rel="stylesheet">    --}}
     <link href="{{ asset('css/invoice.css') }}" rel="stylesheet">
